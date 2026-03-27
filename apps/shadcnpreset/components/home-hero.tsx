28f48b45fde428ad3ffb14053308a9c6f051afae
morganfeeney <info@morganfeeney.com>
2026-03-27 08:36:46 +0000

    Add utils from shadcn

--- apps/shadcnpreset/components/home-hero.tsx
+++ apps/shadcnpreset/components/home-hero.tsx
@@ -1,9 +1,6 @@
 "use client"
 
 import Link from "next/link"
-
-import {Button} from "@/components/ui/button"
-
 import { PresetForm } from "@/components/preset-form"
 import { buttonVariants } from "@/components/ui/button"
 import { PageHeader, PageHeaderDescription, PageHeaderHeading, PageActions } from "@/components/page-header"
