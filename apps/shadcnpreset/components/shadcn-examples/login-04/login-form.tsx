b523e38d3ce81e738a8641ae4609659d53031207
morganfeeney <info@morganfeeney.com>
2026-09-07 08:46:01 +0100

    Ensure light mode images are displayed correctly

--- apps/shadcnpreset/components/shadcn-examples/login-04/login-form.tsx
+++ apps/shadcnpreset/components/shadcn-examples/login-04/login-form.tsx
@@ -97,7 +97,7 @@ export function LoginForm({ className, ...props }: ComponentProps<"div">) {
               fill
               src="https://images.unsplash.com/photo-1569605803663-e9337d901ff9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt=""
-              className="relative z-20 w-full object-cover brightness-60 grayscale"
+              className="relative z-20 w-full object-cover brightness-50 invert dark:brightness-60 dark:grayscale dark:invert-0"
               sizes="(min-width: 768px) 50vw, 0"
             />
           </div>
